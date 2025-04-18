cbe8b39f3cc0af9215bb922686e2261503d8d386
Mohamed Bacar <abdoulandhum@outlook.fr>
2025-04-18 22:49:58 +0200

    version 2.1

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def get_french_sentence():
     return "Erreur: Impossible de récupérer une phrase."
 
 # Fonction pour sauvegarder dans MongoDB
-def save_to_mongo(french_sentence, comorian_translation):
+def save_to_mongo(french_sentence, comorian_translation, username):
     doc = {
         "french_sentence": french_sentence,
-        "comorian_translation": comorian_translation
+        "comorian_translation": comorian_translation,
+        "username": username
     }
     collection.insert_one(doc)
 
@@ -34,6 +35,9 @@ def main():
     if 'french_sentence' not in st.session_state:
         st.session_state.french_sentence = get_french_sentence()
 
+    # Champ pour le nom d'utilisateur
+    username = st.text_input("Entrez votre nom d'utilisateur", key="username_input")
+
     # Afficher la phrase française
     st.write(f"Phrase en français : {st.session_state.french_sentence}")
 
@@ -41,16 +45,18 @@ def main():
     comorian_translation = st.text_area("Fasiri shiKomori", value="", key=f"translation_{st.session_state.french_sentence}")
 
     if st.button('Soumettre'):
-        if comorian_translation:
+        if not username:
+            st.error("Veuillez entrer un nom d'utilisateur.")
+        elif not comorian_translation:
+            st.error("Veuillez entrer une traduction.")
+        else:
             # Sauvegarder dans MongoDB
-            save_to_mongo(st.session_state.french_sentence, comorian_translation)
+            save_to_mongo(st.session_state.french_sentence, comorian_translation, username)
             st.success("Traduction soumise avec succès !")
             # Réinitialiser la phrase française
             st.session_state.french_sentence = get_french_sentence()
             # Forcer le rechargement pour vider le champ et afficher la nouvelle phrase
             st.rerun()
-        else:
-            st.error("Veuillez entrer une traduction.")
 
 if __name__ == '__main__':
     main()

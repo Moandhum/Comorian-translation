7565d59b33fca0bda4a7ecff25aa269fd44236e3
Mohamed Bacar <abdoulandhum@outlook.fr>
2025-04-19 11:27:08 +0200

    clean version

--- tester_mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb+srv://moandhum:Mybdd02@cluster0.0kzbcne.mongodb.net/Cluster0?retryWrites=true&w=majority")
-db = client["Cluster0"]
-collection = db["translations"]
-print(client.list_database_names())
-collection.insert_one({"test": "connexion réussie"})
-print("Insertion réussie")
